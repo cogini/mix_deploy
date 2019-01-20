a705d86be3142b70fb71375f4d7a26bfcffda13a
Jake Morrison <jake@cogini.com>
2019-01-20 21:43:26 +0800

    Docs

--- mix.exs
+++ mix.exs
@@ -6,8 +6,11 @@ defmodule MixDeploy.MixProject do
       app: :mix_deploy,
       version: "0.1.0",
       elixir: "~> 1.6",
-      start_permanent: Mix.env() == :prod,
-      deps: deps(),
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      description: description(),
+      package: package(),
+      source_url: "https://github.com/cogini/mix_deploy",
       dialyzer: [
         plt_add_apps: [:mix, :eex]
         # plt_add_deps: true,
@@ -15,6 +18,8 @@ defmodule MixDeploy.MixProject do
         # flags: ["-Wunmatched_returns", :error_handling, :race_conditions, :underspecs],
         # ignore_warnings: "dialyzer.ignore-warnings"
       ],
+      deps: deps(),
+      docs: docs()
     ]
   end
 
@@ -28,10 +33,31 @@ defmodule MixDeploy.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, "~> 0.19.2", only: :dev, runtime: false},
       {:mix_systemd, git: "https://github.com/cogini/mix_systemd.git"},
       # {:dialyxir, "~> 0.5.1", only: [:dev, :test], runtime: false},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
   end
+
+  defp description do
+    "Generates deployment scripts from template."
+  end
+
+  defp package do
+    [
+      maintainers: ["Jake Morrison"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/cogini/mix_deploy"}
+    ]
+  end
+
+  defp docs do
+    [
+      source_url: "https://github.com/cogini/mix_deploy",
+      extras: ["README.md"]
+    ]
+  end
+
 end
