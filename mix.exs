1622e8d0ec01bb56581979ceb9a32579cb304c1e
Jake Morrison <jake@cogini.com>
2019-02-27 23:27:48 -0800

    Use mix_systemd from hex

--- mix.exs
+++ mix.exs
@@ -34,7 +34,8 @@ defmodule MixDeploy.MixProject do
   defp deps do
     [
       {:ex_doc, "~> 0.19.2", only: :dev, runtime: false},
-      {:mix_systemd, git: "https://github.com/cogini/mix_systemd.git"},
+      # {:mix_systemd, git: "https://github.com/cogini/mix_systemd.git"},
+      {:mix_systemd, "~> 0.1.0"}
       # {:dialyxir, "~> 0.5.1", only: [:dev, :test], runtime: false},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
