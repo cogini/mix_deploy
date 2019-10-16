28c272757b19d10b67e44826ba3f38f3cacd911f
Jake Morrison <jake@cogini.com>
2019-10-16 14:06:42 +0800

    Formatting

--- mix.exs
+++ mix.exs
@@ -6,8 +6,8 @@ defmodule MixDeploy.MixProject do
       app: :mix_deploy,
       version: "0.5.0",
       elixir: "~> 1.6",
-      build_embedded: Mix.env == :prod,
-      start_permanent: Mix.env == :prod,
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
       description: description(),
       package: package(),
       source_url: "https://github.com/cogini/mix_deploy",
@@ -38,8 +38,6 @@ defmodule MixDeploy.MixProject do
       {:mix_systemd, git: "https://github.com/cogini/mix_systemd.git"},
       # {:mix_systemd, "~> 0.1.0"}
       # {:dialyxir, "~> 0.5.1", only: [:dev, :test], runtime: false},
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
   end
 
