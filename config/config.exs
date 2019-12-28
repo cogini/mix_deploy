853defe46ffea78981de25b64f39f6df7e44a0c6
Jake Morrison <jake@cogini.com>
2019-12-28 12:23:50 +0800

    Testing

--- config/config.exs
+++ config/config.exs
@@ -21,6 +21,9 @@ use Mix.Config
 #     config :logger, level: :info
 #
 
+# config :mix_deploy,
+#   base_dir: "/tmp"
+
 # It is also possible to import configuration files, relative to this
 # directory. For example, you can emulate configuration per environment
 # by uncommenting the line below and defining dev.exs, test.exs and such.
