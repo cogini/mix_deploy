52b08520bf72c0d481c77d5871217a7d8e0a25a5
Jake Morrison <jake@cogini.com>
2019-12-28 12:23:07 +0800

    Disable non-functional test

--- test/mix_deploy_test.exs
+++ test/mix_deploy_test.exs
@@ -2,7 +2,7 @@ defmodule MixDeployTest do
   use ExUnit.Case
   doctest MixDeploy
 
-  test "greets the world" do
-    assert MixDeploy.hello() == :world
-  end
+  # test "greets the world" do
+  #   assert MixDeploy.hello() == :world
+  # end
 end
