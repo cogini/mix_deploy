dc8dfe8b499cf074dd9e386304bcaa14ec6dba64
Steffan Levet <steffan@narrative.so>
2020-04-02 16:45:07 +1300

    remove old datadog script; add beats rotation script

--- lib/mix/tasks/deploy.ex
+++ lib/mix/tasks/deploy.ex
@@ -7,29 +7,34 @@ defmodule Mix.Tasks.Deploy do
 
   alias MixDeploy.User
 
-  @spec parse_args(OptionParser.argv()) :: Keyword.t
+  @spec parse_args(OptionParser.argv()) :: Keyword.t()
   def parse_args(argv) do
     opts = [
       strict: [
-        version: :string,
+        version: :string
       ]
     ]
+
     {overrides, _} = OptionParser.parse!(argv, opts)
 
     mix_config = Mix.Project.config()
     user_config = Application.get_all_env(:mix_deploy)
 
     app_name = mix_config[:app]
-    ext_name = app_name
-               |> to_string
-               |> String.replace("_", "-")
+
+    ext_name =
+      app_name
+      |> to_string
+      |> String.replace("_", "-")
+
     service_name = ext_name
 
-    module_name = app_name
-                  |> to_string
-                  |> String.split("_")
-                  |> Enum.map(&String.capitalize/1)
-                  |> Enum.join("")
+    module_name =
+      app_name
+      |> to_string
+      |> String.split("_")
+      |> Enum.map(&String.capitalize/1)
+      |> Enum.join("")
 
     base_dir = user_config[:base_dir] || "/srv"
 
@@ -86,20 +91,23 @@ defmodule Mix.Tasks.Deploy do
       conform_conf_path: "/etc/#{ext_name}/#{app_name}.conf",
 
       # Target systemd version
-      systemd_version: 219, # CentOS 7
+      # CentOS 7
+      systemd_version: 219,
       # systemd_version: 229, # Ubuntu 16.04
 
       # Whether to create /etc/suders.d file allowing deploy an/or app user to
       # restart app
       sudo_deploy: false,
       sudo_app: false,
-
-      restart_method: :systemctl, # :systemd_flag | :systemctl | :touch
-      runtime_environment_service: false, # enable and start app runtime-environment.service
-
+      # :systemd_flag | :systemctl | :touch
+      restart_method: :systemctl,
+      # enable and start app runtime-environment.service
+      runtime_environment_service: false,
       dirs: [
-        :runtime,         # RELEASE_TMP, RELEASE_MUTABLE_DIR, runtime-environment
-        :configuration,   # Config files, Erlang cookie
+        # RELEASE_TMP, RELEASE_MUTABLE_DIR, runtime-environment
+        :runtime,
+        # Config files, Erlang cookie
+        :configuration
         # :logs,          # External log file, not journald
         # :cache,         # App cache files which can be deleted
         # :state,         # App state persisted between runs
@@ -133,7 +141,6 @@ defmodule Mix.Tasks.Deploy do
       tmp_directory: service_name,
       tmp_directory_base: "/var/tmp",
       tmp_directory_mode: "750",
-
       templates: [
         "deploy-clean-target",
         "deploy-copy-files",
@@ -157,42 +164,50 @@ defmodule Mix.Tasks.Deploy do
         "deploy-sync-config-s3",
         "deploy-sync-assets-s3",
         "deploy-noop",
-        "deploy-datadog"
+        "deploy-rotate-beats-secrets"
       ]
     ]
 
-    cfg = defaults
-             |> Keyword.merge(user_config)
-             |> Keyword.merge(overrides)
+    cfg =
+      defaults
+      |> Keyword.merge(user_config)
+      |> Keyword.merge(overrides)
 
     # Default OS user and group names
-    cfg = Keyword.merge([
-      app_user: cfg[:deploy_user],
-      app_group: cfg[:deploy_group],
-    ], cfg)
+    cfg =
+      Keyword.merge(
+        [
+          app_user: cfg[:deploy_user],
+          app_group: cfg[:deploy_group]
+        ],
+        cfg
+      )
 
     # Mix.shell.info "cfg: #{inspect cfg}"
 
     # Data calculated from other things
-    Keyword.merge([
-      releases_dir: Path.join(cfg[:deploy_dir], "releases"),
-      scripts_dir: Path.join(cfg[:deploy_dir], "bin"),
-      flags_dir: Path.join(cfg[:deploy_dir], "flags"),
-      current_dir: Path.join(cfg[:deploy_dir], "current"),
-
-      bin_dir: if cfg[:output_dir_per_env] do
-        Path.join(cfg[:output_dir], "bin")
-      else
-        "bin"
-      end,
-
-      runtime_dir: Path.join(cfg[:runtime_directory_base], cfg[:runtime_directory]),
-      configuration_dir: Path.join(cfg[:configuration_directory_base], cfg[:configuration_directory]),
-      logs_dir: Path.join(cfg[:logs_directory_base], cfg[:logs_directory]),
-      tmp_dir: Path.join(cfg[:tmp_directory_base], cfg[:tmp_directory]),
-      state_dir: Path.join(cfg[:state_directory_base], cfg[:state_directory]),
-      cache_dir: Path.join(cfg[:cache_directory_base], cfg[:cache_directory]),
-    ], cfg)
+    Keyword.merge(
+      [
+        releases_dir: Path.join(cfg[:deploy_dir], "releases"),
+        scripts_dir: Path.join(cfg[:deploy_dir], "bin"),
+        flags_dir: Path.join(cfg[:deploy_dir], "flags"),
+        current_dir: Path.join(cfg[:deploy_dir], "current"),
+        bin_dir:
+          if cfg[:output_dir_per_env] do
+            Path.join(cfg[:output_dir], "bin")
+          else
+            "bin"
+          end,
+        runtime_dir: Path.join(cfg[:runtime_directory_base], cfg[:runtime_directory]),
+        configuration_dir:
+          Path.join(cfg[:configuration_directory_base], cfg[:configuration_directory]),
+        logs_dir: Path.join(cfg[:logs_directory_base], cfg[:logs_directory]),
+        tmp_dir: Path.join(cfg[:tmp_directory_base], cfg[:tmp_directory]),
+        state_dir: Path.join(cfg[:state_directory_base], cfg[:state_directory]),
+        cache_dir: Path.join(cfg[:cache_directory_base], cfg[:cache_directory])
+      ],
+      cfg
+    )
   end
 end
 
@@ -220,7 +235,6 @@ defmodule Mix.Tasks.Deploy.Init do
     :ok = File.mkdir_p(template_dir)
     {:ok, _files} = File.cp_r(app_dir, template_dir)
   end
-
 end
 
 defmodule Mix.Tasks.Deploy.Generate do
@@ -239,11 +253,14 @@ defmodule Mix.Tasks.Deploy.Generate do
 
   def flags_dir(cfg) do
     if cfg[:restart_method] in [:systemd_flag, :touch] do
-      perms = if cfg[:restart_method] == :touch do
-        0o770 # app needs to be able to delete file at runtime
-      else
-        0o750
-      end
+      perms =
+        if cfg[:restart_method] == :touch do
+          # app needs to be able to delete file at runtime
+          0o770
+        else
+          0o750
+        end
+
       [{cfg[:flags_dir], cfg[:deploy_user], cfg[:app_group], perms, "Flag files"}]
     else
       []
@@ -262,38 +279,48 @@ defmodule Mix.Tasks.Deploy.Generate do
     # app_group = cfg[:app_group]
 
     create_flags_dir = cfg[:restart_method] in [:systemd_flag, :touch]
-    flags_dir_perms = if cfg[:restart_method] == :touch do
-      0o770 # app needs to be able to delete the flag file at runtime
-    else
-      0o750
-    end
 
-    dirs = [
-      {true, cfg[:deploy_dir], "$DEPLOY_USER", "$APP_GROUP", 0o750, "Base dir"},
-      {true, cfg[:releases_dir], "$DEPLOY_USER", "$APP_GROUP", 0o750, "Releases"},
-      {true, cfg[:scripts_dir], "$DEPLOY_USER", "$APP_GROUP", 0o750, "Target scripts"},
-      {create_flags_dir, cfg[:flags_dir], "$DEPLOY_USER", "$APP_GROUP", flags_dir_perms, "Flag files"}
-    ] ++
-    if cfg[:systemd_version] < 235 do
-      # systemd will automatically create directories in newer versions
-      # https://www.freedesktop.org/software/systemd/man/systemd.exec.html#RuntimeDirectory=
+    flags_dir_perms =
+      if cfg[:restart_method] == :touch do
+        # app needs to be able to delete the flag file at runtime
+        0o770
+      else
+        0o750
+      end
+
+    dirs =
       [
-        # We always need runtime dir, as we use it for RELEASE_TMP
-        {true, cfg[:runtime_dir], "$APP_USER", "$APP_GROUP", 0o750, "systemd RuntimeDirectory"},
+        {true, cfg[:deploy_dir], "$DEPLOY_USER", "$APP_GROUP", 0o750, "Base dir"},
+        {true, cfg[:releases_dir], "$DEPLOY_USER", "$APP_GROUP", 0o750, "Releases"},
+        {true, cfg[:scripts_dir], "$DEPLOY_USER", "$APP_GROUP", 0o750, "Target scripts"},
+        {create_flags_dir, cfg[:flags_dir], "$DEPLOY_USER", "$APP_GROUP", flags_dir_perms,
+         "Flag files"}
+      ] ++
+        if cfg[:systemd_version] < 235 do
+          # systemd will automatically create directories in newer versions
+          # https://www.freedesktop.org/software/systemd/man/systemd.exec.html#RuntimeDirectory=
+          [
+            # We always need runtime dir, as we use it for RELEASE_TMP
+            {true, cfg[:runtime_dir], "$APP_USER", "$APP_GROUP", 0o750,
+             "systemd RuntimeDirectory"},
 
-        # Needed for exernal config file
-        {:configuration in cfg[:dirs], cfg[:configuration_dir], "$DEPLOY_USER", "$APP_GROUP", 0o750, "systemd ConfigurationDirectory"},
+            # Needed for exernal config file
+            {:configuration in cfg[:dirs], cfg[:configuration_dir], "$DEPLOY_USER", "$APP_GROUP",
+             0o750, "systemd ConfigurationDirectory"},
+            {:logs in cfg[:dirs], cfg[:logs_dir], "$APP_USER", "$APP_GROUP", 0o700,
+             "systemd LogsDirectory"},
+            {:state in cfg[:dirs], cfg[:state_dir], "$APP_USER", "$APP_GROUP", 0o700,
+             "systemd StateDirectory"},
+            {:cache in cfg[:dirs], cfg[:cache_dir], "$APP_USER", "$APP_GROUP", 0o700,
+             "systemd CacheDirectory"},
 
-        {:logs  in cfg[:dirs], cfg[:logs_dir],  "$APP_USER", "$APP_GROUP", 0o700, "systemd LogsDirectory"},
-        {:state in cfg[:dirs], cfg[:state_dir], "$APP_USER", "$APP_GROUP", 0o700, "systemd StateDirectory"},
-        {:cache in cfg[:dirs], cfg[:cache_dir], "$APP_USER", "$APP_GROUP", 0o700, "systemd CacheDirectory"},
-
-        # Better handled by PrivateTmp in newer systemd
-        {:tmp in cfg[:dirs], cfg[:tmp_dir], "$APP_USER", "$APP_GROUP", 0o700, "Temp directory"},
-      ]
-    else
-      []
-    end
+            # Better handled by PrivateTmp in newer systemd
+            {:tmp in cfg[:dirs], cfg[:tmp_dir], "$APP_USER", "$APP_GROUP", 0o700,
+             "Temp directory"}
+          ]
+        else
+          []
+        end
 
     files = [
       # {"bin/deploy", Path.join(cfg[:scripts_dir], "deploy"), "$DEPLOY_USER", "$APP_GROUP", 0o750},
@@ -311,11 +338,12 @@ defmodule Mix.Tasks.Deploy.Generate do
     end
   end
 
-  defp write_template(cfg, dest_dir, template), do: write_template(cfg, dest_dir, template, template)
+  defp write_template(cfg, dest_dir, template),
+    do: write_template(cfg, dest_dir, template, template)
 
   defp write_template(cfg, dest_dir, template, file) do
     target_file = Path.join(dest_dir, file)
-    Mix.shell.info "Generating #{target_file} from template #{template}"
+    Mix.shell().info("Generating #{target_file} from template #{template}")
     Templates.write_template(cfg, dest_dir, template, file)
   end
 end
@@ -347,30 +375,45 @@ defmodule Mix.Tasks.Deploy.Local do
     deploy_release(config)
   end
 
-  @spec deploy_release(Keyword.t) :: no_return
+  @spec deploy_release(Keyword.t()) :: no_return
   def deploy_release(cfg) do
     release_dir = Path.join(cfg[:releases_dir], create_timestamp())
-    Mix.shell.info "Deploying release to #{release_dir}"
+    Mix.shell().info("Deploying release to #{release_dir}")
     :ok = File.mkdir_p(release_dir)
 
     app = to_string(cfg[:app_name])
-    tar_file = Path.join([cfg[:build_path], "rel", app, "releases", cfg[:version], "#{app}.tar.gz"])
-    Mix.shell.info "Extracting tar #{tar_file}"
+
+    tar_file =
+      Path.join([cfg[:build_path], "rel", app, "releases", cfg[:version], "#{app}.tar.gz"])
+
+    Mix.shell().info("Extracting tar #{tar_file}")
     :ok = :erl_tar.extract(to_charlist(tar_file), [{:cwd, release_dir}, :compressed])
 
     current_link = cfg[:current_path]
+
     if File.exists?(current_link) do
       :ok = File.rm(current_link)
     end
+
     :ok = File.ln_s(release_dir, current_link)
   end
 
   def create_timestamp do
-    {{year, month, day}, {hour, minute, second}} = :calendar.now_to_universal_time(:os.timestamp())
-    timestamp = :io_lib.format("~4..0B~2..0B~2..0B~2..0B~2..0B~2..0B", [year, month, day, hour, minute, second])
-    timestamp |> List.flatten |> to_string
-  end
+    {{year, month, day}, {hour, minute, second}} =
+      :calendar.now_to_universal_time(:os.timestamp())
 
+    timestamp =
+      :io_lib.format("~4..0B~2..0B~2..0B~2..0B~2..0B~2..0B", [
+        year,
+        month,
+        day,
+        hour,
+        minute,
+        second
+      ])
+
+    timestamp |> List.flatten() |> to_string
+  end
 end
 
 defmodule Mix.Tasks.Deploy.Local.Rollback do
@@ -383,32 +426,34 @@ defmodule Mix.Tasks.Deploy.Local.Rollback do
   def run(args) do
     cfg = Mix.Tasks.Deploy.parse_args(args)
 
-    dirs = cfg[:releases_path] |> File.ls! |> Enum.sort |> Enum.reverse
+    dirs = cfg[:releases_path] |> File.ls!() |> Enum.sort() |> Enum.reverse()
 
     rollback(dirs, cfg)
   end
 
-  @spec rollback([Path.t], Keyword.t) :: :ok
+  @spec rollback([Path.t()], Keyword.t()) :: :ok
   defp rollback([_current, prev | _rest], cfg) do
     release_path = Path.join(cfg[:releases_path], prev)
     current_dir = cfg[:current_dir]
-    Mix.shell.info "Making link from #{release_path} to #{current_dir}"
+    Mix.shell().info("Making link from #{release_path} to #{current_dir}")
     :ok = remove_link(current_dir)
     :ok = File.ln_s(release_path, current_dir)
   end
+
   defp rollback(dirs, _cfg) do
-    Mix.shell.info "Nothing to roll back to: releases = #{inspect dirs}"
+    Mix.shell().info("Nothing to roll back to: releases = #{inspect(dirs)}")
     :ok
   end
 
-  @spec remove_link(Path.t) :: :ok | {:error, :file.posix()}
+  @spec remove_link(Path.t()) :: :ok | {:error, :file.posix()}
   defp remove_link(current_path) do
     case File.read_link(current_path) do
       {:ok, target} ->
-        Mix.shell.info "Removing link from #{target} to #{current_path}"
+        Mix.shell().info("Removing link from #{target} to #{current_path}")
         File.rm(current_path)
+
       {:error, _reason} ->
-        Mix.shell.info "No current link #{current_path}"
+        Mix.shell().info("No current link #{current_path}")
         :ok
     end
   end
