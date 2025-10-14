a7207da4590205bf9208e1942a6aa65dd0f5d5b5
Jake Morrison <jake@cogini.com>
2025-10-14 16:28:20 -0500

    Update libs

--- mix.exs
+++ mix.exs
@@ -59,12 +59,13 @@ defmodule MixDeploy.MixProject do
       {:castore, "~> 1.0", only: [:dev, :test], runtime: false},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.2", only: [:dev, :test], runtime: false},
-      {:ex_doc, "~> 0.33.0", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.38.0", only: :dev, runtime: false},
       {:excoveralls, "~> 0.18.0", only: [:dev, :test], runtime: false},
       {:junit_formatter, "~> 3.3", only: [:dev, :test], runtime: false},
       {:mix_audit, "~> 2.0", only: [:dev, :test], runtime: false},
-      {:mix_systemd, "~> 0.8"},
-      {:styler, "~> 0.11.0", only: [:dev, :test], runtime: false}
+      # {:mix_systemd, "~> 0.8"},
+      {:mix_systemd, github: "cogini/mix_systemd"},
+      {:styler, "~> 1.9.0", only: [:dev, :test], runtime: false}
     ]
   end
 
